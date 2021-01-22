d6f2588584cb6b0839e8d91521e8e20005cec1f7
zhangjuan <zhangjuaner777@163.com>
2021-01-22 14:09:20 +0800

    new

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/router'
+import store from './store/store'
+import api from './api/api'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = api
 
 new Vue({
   router,
